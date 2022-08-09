45a049acc7422d5671a1a2cf3cd76e6dc714480f
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-08-09 18:08:51 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int turkce,matematik,fen,sosyal,beden;
+        double ortalama;
+
+        System.out.print("Turkce Notunuzu Giriniz :");
+        turkce = input.nextInt();
+        System.out.print("Matematik Notunuzu Giriniz :");
+        matematik = input.nextInt();
+        System.out.print("Fen Bilgisi Notunuzu Giriniz :");
+        fen = input.nextInt();
+        System.out.print("Sosyal Bilgiler Notunuzu Giriniz :");
+        sosyal = input.nextInt();
+        System.out.print("Beden Egitimi Notunuzu Giriniz :");
+        beden = input.nextInt();
+
+        ortalama = (turkce + matematik + fen + sosyal + beden) / 5;
+        System.out.println("Not Ortalamasi :" + ortalama);
+
+        if (ortalama>=50) {
+            System.out.println("Sinifi Gectiniz");
+        }
+        else {
+            System.out.println("Sinifta Kaldiniz");
+        }
+
     }
 }
